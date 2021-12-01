dee92f4d8604477f5e669943c766f3d9fc1f0f72
matt-connell <68166197+matt-connell@users.noreply.github.com>
2021-12-01 01:45:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function changeBackgroundLight(){
+    document.getElementById('switch').style.backgroundColor = 'lightblue';
+    document.getElementById("myH").style.color = "rgb(72,72,72)";
+    document.getElementById("myH1").style.color = "rgb(72,72,72)";
+    document.getElementById("myH2").style.color = "rgb(72,72,72)";
+    document.getElementById("myH3").style.color = "rgb(72,72,72)";
+
+
+}
+
+function changeBackgroundDark(){
+    document.getElementById('switch').style.backgroundColor = 'white';
+    document.getElementById("myH").style.color = "white";
+    document.getElementById("myH1").style.color = "white";
+    document.getElementById("myH2").style.color = "white";
+    document.getElementById("myH3").style.color = "white";
+
+}
