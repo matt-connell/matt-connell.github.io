7f38ee14b9733d0519db8b2a5681839a7e0a4b89
mattycraps <matt.connell02@gmail.com>
2021-12-01 23:06:19 -0500

    light and dark css modes

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-function changeBackgroundLight(){
-    document.getElementById('switch').style.backgroundColor = 'lightblue';
-    document.getElementById("myH").style.color = "rgb(72,72,72)";
-    document.getElementById("myH1").style.color = "rgb(72,72,72)";
-    document.getElementById("myH2").style.color = "rgb(72,72,72)";
-    document.getElementById("myH3").style.color = "rgb(72,72,72)";
+function toggleTheme() { 
+    // Obtains an array of all <link> 
+    // elements. 
+    // Select your element using indexing. 
+    var theme = document.getElementsByTagName('link')[0]; 
 
-
-}
-
-function changeBackgroundDark(){
-    document.getElementById('switch').style.backgroundColor = 'white';
-    document.getElementById("myH").style.color = "white";
-    document.getElementById("myH1").style.color = "white";
-    document.getElementById("myH2").style.color = "white";
-    document.getElementById("myH3").style.color = "white";
-
+    // Change the value of href attribute  
+    // to change the css sheet. 
+    if (theme.getAttribute('href') == 'dark-theme.css') { 
+        theme.setAttribute('href', 'light-theme.css'); 
+    } else { 
+        theme.setAttribute('href', 'dark-theme.css'); 
+    } 
 }
