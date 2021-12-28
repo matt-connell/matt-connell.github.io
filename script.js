0327a27153f88a75630e56e72eb2ac474e7115ca
mattycraps <matt.connell02@gmail.com>
2021-12-28 16:25:10 -0500

    Added new features
    back to top bar
    edited the contact section

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ function toggleTheme() {
         theme.setAttribute('href', 'dark-theme.css'); 
     } 
 }
+
+
+var topBtn = document.getElementById("top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+    if (document.body.scrollTop() > 20 || document.documentElement.scrollTop > 20){
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+}
+
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
